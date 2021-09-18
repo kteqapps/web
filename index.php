e550f95907b7d194c9738d1eb8cccdd1f9e92be1
kteqapps <mkishea12@gmail.com>
2021-09-18 19:50:17 +0300

    kteqapps website first commit

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kteqapps - Think Ahead</title>
+    <meta name="description" content="kteqapps provides software solutions for businesses as either mobile, desktop, web or embedded.
+    We also offer consultancy in business software developent and payment solutions">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta property="og:description" content="kteqapps provides software solutions for businesses as either mobile, desktop, web or embedded.
+    We also offer consultancy in business software developent and payment solutions">
+    <meta property="og:url" content="https://kteqapps.com">
+    <meta property="og:image" content="favicon.icns">
+    <meta property="og:site_name" content="Kteqapps Developers">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="canonical" href="https://kteqapps.com">
+    <link rel="search" type="application/opensearchdescription+xml" title="kteqapps" href="opensearch.xml">Î
+    <meta name=viewport content="width=device-width, initial-scale=1">
+
+
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <meta name="msapplication-config" content="/browserconfig.xml" />
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
+
+
+    <meta charset="utf-8">
+    <style type="text/css">
+        .animate-bounce {
+            animation: bounce 5s infinite !important;
+
+            @keyframes bounce {
+
+                0%,
+                100% {
+                    transform: translateY(-25%);
+                    animation-timing-function: cubic-bezier(0.8, 0, 1, 1);
+                }
+
+                50% {
+                    transform: translateY(0);
+                    animation-timing-function: cubic-bezier(0, 0, 0.2, 1);
+                }
+            }
+        }
+    </style>
+    <?php
+    /*
+        <!-- Font Awesome if you need it
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">  
+        -->  
+    */ ?>
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css">
+    <?php /*
+    <!--Replace with your tailwind.css once created-->*/ ?>
+</head>
+
+<body class="font-sans antialiased text-yellow-900 leading-normal tracking-wider bg-cover" style="background-image:url('background.jpg');">
+    <div class="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
+        <!--Main Col-->
+        <div id="profile" class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
+            <div class="p-4 md:p-12 text-center lg:text-left">
+                <!-- Image for mobile view-->
+                <div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style="background-image: url('background.jpg')"></div>
+
+                <h1 class="text-3xl font-bold pt-8 lg:pt-0">Hi, welcome to kteqapps</h1>
+                <div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-blue-500 opacity-25"></div>
+                <p class="pt-2 text-base font-bold flex items-center justify-center lg:justify-start">
+                    <svg class="h-8 fill-current text-blue-700 pr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z" />
+                    </svg> Custom software solutions for business
+                </p>
+
+                <p class="pt-2 text-yellow-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start">
+                    <svg class="h-8 fill-current text-blue-700 pr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z" />
+                    </svg> Based - in Kampala, Uganda
+                </p>
+
+                <p class="pt-8 text-md">
+                    We build, monitor, host and maintain custom or open-source business software solutions for you.
+                </p>
+                <p class="pt-8 text-sm">
+                    &mdash; Think ahead!
+                </p>
+
+                <div class="pt-12 pb-8">
+                    <a href="mailto:support@kteqapps.com" class="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full">
+                        Get In Touch
+                    </a>
+                </div>
+
+                <div class="mt-6 pb-16 lg:pb-0 w-4/5 lg:w-full mx-auto flex flex-wrap items-center justify-between">
+                    <a class="link" href="#" data-tippy-content="@facebook_handle">
+                        <svg class="h-6 fill-current text-yellow-600 hover:text-blue-700" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <title>Facebook</title>
+                            <path d="M22.676 0H1.324C.593 0 0 .593 0 1.324v21.352C0 23.408.593 24 1.324 24h11.494v-9.294H9.689v-3.621h3.129V8.41c0-3.099 1.894-4.785 4.659-4.785 1.325 0 2.464.097 2.796.141v3.24h-1.921c-1.5 0-1.792.721-1.792 1.771v2.311h3.584l-.465 3.63H16.56V24h6.115c.733 0 1.325-.592 1.325-1.324V1.324C24 .593 23.408 0 22.676 0" />
+                        </svg>
+                    </a>
+                    <a class="link" href="#" data-tippy-content="https://twitter.com/kteqapps"><svg class="h-6 fill-current text-yellow-600 hover:text-blue-700" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <title>Twitter</title>
+                            <path d="M23.954 4.569c-.885.389-1.83.654-2.825.775 1.014-.611 1.794-1.574 2.163-2.723-.951.555-2.005.959-3.127 1.184-.896-.959-2.173-1.559-3.591-1.559-2.717 0-4.92 2.203-4.92 4.917 0 .39.045.765.127 1.124C7.691 8.094 4.066 6.13 1.64 3.161c-.427.722-.666 1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693 4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604 3.417-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557 2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56 2.46-2.548l-.047-.02z" />
+                        </svg></a>
+
+                    <a class="link" href="#" data-tippy-content="https://www.youtube.com/channel/UC7WZCCaNA2g4IAXN7FRwzsw">
+                        <svg class="h-6 fill-current text-yellow-600 hover:text-blue-700" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <title>YouTube</title>
+                            <path d="M23.495 6.205a3.007 3.007 0 0 0-2.088-2.088c-1.87-.501-9.396-.501-9.396-.501s-7.507-.01-9.396.501A3.007 3.007 0 0 0 .527 6.205a31.247 31.247 0 0 0-.522 5.805 31.247 31.247 0 0 0 .522 5.783 3.007 3.007 0 0 0 2.088 2.088c1.868.502 9.396.502 9.396.502s7.506 0 9.396-.502a3.007 3.007 0 0 0 2.088-2.088 31.247 31.247 0 0 0 .5-5.783 31.247 31.247 0 0 0-.5-5.805zM9.609 15.601V8.408l6.264 3.602z" />
+                        </svg></a>
+                </div>
+                <?php /*
+
+                <!-- Use https://simpleicons.org/ to find the svg for your preferred product -->
+*/ ?>
+            </div>
+
+        </div>
+
+        <!--Img Col-->
+        <div class="w-full lg:w-2/5">
+            <?php /*
+
+        <!-- Big profile image for side bar (desktop) --> */ ?>
+            <img src="current.jpg" class="rounded-none animate-bounce infinite lg:rounded-lg lg:rounded-l-none shadow-2xl hidden lg:block">
+            <?php /*
+  <!-- Image from: http://unsplash.com/photos/MP0IUfwrn0A --> */ ?>
+        </div>
+
+        <!-- Pin to top right corner -->
+        <div class="absolute top-0 right-0 h-12 w-18 p-4">
+            <button class="js-change-theme focus:outline-none">🌙</button>
+        </div>
+
+    </div>
+
+    <script src="https://unpkg.com/popper.js@1/dist/umd/popper.min.js"></script>
+    <script src="https://unpkg.com/tippy.js@4"></script>
+
+    <script>
+        //Init tooltips
+        tippy('.link', {
+            placement: 'bottom'
+        })
+
+        //Toggle mode
+        const toggle = document.querySelector('.js-change-theme');
+        const body = document.querySelector('body');
+        const profile = document.getElementById('profile');
+
+        function getRandomColor(count) {
+            var items = ['blue','yellow', 'pink'];
+            var item = items[count];
+            return item;
+        }
+
+        var counter = 0;
+
+        function changeColor() {
+            var color = getRandomColor(counter);
+            if (counter == 3) {
+                counter = 0; //reset to start
+            } else {
+                ++counter; //iterate to next image
+            };
+            if (body.classList.contains('text-' + color + '-900') && counter % 3 == 0) {
+                toggle.innerHTML = "☀️";
+                body.classList.remove('text-' + color + '-900');
+                body.classList.add('text-' + color + '-100');
+                profile.classList.remove('bg-white');
+                profile.classList.add('bg-' + color + '-900');
+            } else {
+                toggle.innerHTML = "🌙";
+                body.classList.remove('text-' + color + '-100');
+                body.classList.add('text-' + color + '-900');
+                profile.classList.remove('bg-' + color + '-900');
+                profile.classList.add('bg-white');
+            }
+        }
+
+        setInterval(changeColor, 500);
+    </script>
+
+</body>
+
+</html>

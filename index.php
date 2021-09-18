a77e101826c1542e35d9da1aeed2941896436a75
kteqapps <mkishea12@gmail.com>
2021-09-18 23:36:15 +0300

    add google analytics

--- index.php
+++ index.php
@@ -57,24 +57,19 @@
     <link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css">
     <?php /*
     <!--Replace with your tailwind.css once created-->*/ ?>
-    <!-- Google Tag Manager -->
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-7MRDHEFYPB"></script>
     <script>
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-PJB2S7Q');
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-7MRDHEFYPB');
     </script>
-    <!-- End Google Tag Manager -->
 </head>
 
 <body class="font-sans antialiased text-yellow-900 leading-normal tracking-wider bg-cover" style="background-image:url('background.jpg');">
